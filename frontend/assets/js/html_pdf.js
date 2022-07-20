e38c465063460b354bf47e389977a88c2ae38422
Leandro Lourenço <leandrollemos712@gmail.com>
2022-07-20 15:58:30 -0300

    Day 6 p3

--- frontend/assets/js/html_pdf.js
+++ frontend/assets/js/html_pdf.js
@@ -1,18 +1,13 @@
 
 window.onload = function () {
 
-    document.getElementById("reload").addEventListener("click", () => {
-        location.reload();
-    });
-
     document.getElementById("btnGerar")
         .addEventListener("click", () => {
+
             btnGerar.style.display = 'none';
-            reload.style.display = 'none';
-            addPrefixoReal();
+
             const body = this.document.getElementById("body");
-            console.log(body);
-            console.log(window);
+
             var opt = {
                 margin: 0,
                 filename: 'Proposta Comercial Iliot.pdf',
@@ -20,7 +15,7 @@ window.onload = function () {
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
             };
+
             html2pdf().from(body).set(opt).save();
-            // btnGerar.style.display = 'inline';
         })
 }
